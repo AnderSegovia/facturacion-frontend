88da983640c436b994aeccb0819270e9255d543a
andercastro <andersteven.castro@gmail.com>
2025-06-25 22:28:19 -0600

    Generacion de facturas

--- src/api.js
+++ src/api.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //  baseURL: 'https://facturacion-backend-92qu.onrender.com/api/', 
-  baseURL: 'http://localhost:3001/api', 
+  baseURL: 'https://facturacion-backend-92qu.onrender.com/api/', 
+  //baseURL: 'http://localhost:3001/api', 
   headers: {
     'Content-Type': 'application/json'
   }
 });
 
 export default api;
-  
